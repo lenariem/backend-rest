b00ed95b9ea67254189723d3e96bef00fa546f33
unknown <olena.denysova88@gmail.com>
2021-01-17 19:35:31 +0100

    cors

--- app.js
+++ app.js
@@ -43,21 +43,21 @@ app.use(express.json()); // parser for JSON data => req.body
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-let frontendOrigin = 'https://frontend-rest-react.herokuapp.com/'
+/* let frontendOrigin = 'https://frontend-rest-react.herokuapp.com/'
 app.use(
   cors({
     origin: [frontendOrigin], // HERE YOU CAN WHITELIST THE DOMAIN OF YOUR CLIENT
     credentials: true, // allow cookies from other origins
   })
-);
+); */
 
-/*
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
     });
-*/
+
 
 /** STATIC FILES */
 app.use(express.static(path.join(__dirname, 'public'))); // => current_folder / public
